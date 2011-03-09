e4dc256703fe716c503303053d390e6467ec62b3
John J. Glynn <jjg4@jjg4server.home>
2011-03-09 02:18:31 -0500

    Removed php-libs, sql, and non used css.

--- sql/init_people.sql
+++
@@ -1,9 +0,0 @@
-USE ritquidditch;
-INSERT INTO people (name,title,email,bigpic,smpic)
-    VALUES( "Mark Oswald", "President", "mxo6749@rit.edu", "images/bgmark.png", DEFAULT );
-INSERT INTO people (name,title,email,bigpic,smpic)
-    VALUES( "Gideon Williams", "Vice President", "gaw5951@rit.edu", "images/bggideon.png", DEFAULT );
-INSERT INTO people (name,title,email,bigpic,smpic)
-    VALUES( "John Glynn", "Treasurer", "jjg1914@rit.edu", "images/bgjohn.png", DEFAULT );
-INSERT INTO people (name,title,email,bigpic,smpic)
-    VALUES( "Amanda Pardee", "Secretary", "aip4537@rit.edu", "images/bgamanda.png", DEFAULT );
